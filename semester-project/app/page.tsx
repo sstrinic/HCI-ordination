4c32807fa5b359b15cd91b7a184fc6dca0651174
sstrinic <stipan.strinic.00@fesb.hr>
2023-12-02 12:09:21 +0100

    added banner img

--- semester-project/app/page.tsx
+++ semester-project/app/page.tsx
@@ -1,26 +1,27 @@
 import Image from "next/image";
+import bannerImg from "./banner.png";
 
-export interface Img {
-  message: string;
-  url: string;
-}
+// export interface Img {
+//   message: string;
+//   url: string;
+// }
 
-const BASE_API_URL = "https://random-d.uk/api/v2/quack";
+// const BASE_API_URL = "https://random-d.uk/api/v2/quack";
 
-const getImg = async (): Promise<Img> => {
-  const data = await fetch(`${BASE_API_URL}`, { next: { revalidate: 300 } });
-  return data.json();
-};
+// const getImg = async (): Promise<Img> => {
+//   const data = await fetch(`${BASE_API_URL}`, { next: { revalidate: 300 } });
+//   return data.json();
+// };
 
-export default async function Home() {
-  const img = await getImg();
+export default function Home() {
+  // const img = await getImg();
 
   return (
     <main className="flex flex-col justify-center">
       <h1 className="flex justify-center p-14 text-4xl font-bold">Home Page</h1>
-      <div className="h-80 w-3/12 relative m-auto max-md:w-full">
+      <div className="w-[1024px] aspect-video relative m-auto max-lg:w-full max-md:w-full">
         <Image
-          src={img.url}
+          src={bannerImg} //{img.url}
           alt="Duck img from public API"
           fill={true}
           object-fit="cover"
