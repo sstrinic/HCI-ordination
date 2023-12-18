cca8b81be7c38b5043d7b70dc86e1347cbd44ede
Stipan Strinic <Stipan.Strinic.00@fesb.hr>
2023-12-18 23:56:56 +0100

    empty page vercel error

---
+++ semester-project/app/(contentful)/cms/blog/page.tsx
@@ -0,0 +1,10 @@
+function Resources() {
+    return (
+      <main className="bg-orange-400">
+        <h1 className="flex justify-center p-14 text-4xl font-bold">Resources</h1>
+      </main>
+    );
+  }
+  
+  export default Resources;
+  
--- semester-project/app/(contentful)/cms/services/page.tsx
+++ semester-project/app/(contentful)/cms/services/page.tsx
@@ -32,3 +32,13 @@
 //   };
   
 //   export default CmsPage;
+function Resources() {
+    return (
+      <main className="bg-orange-400">
+        <h1 className="flex justify-center p-14 text-4xl font-bold">Resources</h1>
+      </main>
+    );
+  }
+  
+  export default Resources;
+  
