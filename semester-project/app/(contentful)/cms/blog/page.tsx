3ec1849611b42bc9a064304d6e61841c2c5e0e87
sstrinic <stipan.strinic.00@fesb.hr>
2024-01-05 22:40:13 +0100

    Blog page test

--- semester-project/app/(contentful)/cms/blog/page.tsx
+++ semester-project/app/(contentful)/cms/blog/page.tsx
@@ -1,10 +1,35 @@
-function Blog() {
-    return (
-      <main className="bg-orange-400 m-auto">
-        <h1 className="flex justify-center p-14 text-4xl font-bold">Blog</h1>
-      </main>
-    );
-  }
-  
-  export default Blog;
-  
+import contentfulService from "@/lib/contentfulClient";
+import { FC } from "react";
+
+export type SearchParams = {
+  searchParams: Record<string, string | string[] | undefined>;
+};
+
+const CmsPage: FC<SearchParams> = async ({ searchParams }) => {
+  const products = await contentfulService.getAllPosts();
+
+  const filteredProducts = searchParams._category
+    ? products.filter((product) => {
+        return product.categories
+    })
+    : products;
+
+  return (
+    <main className="container flex flex-col items-center gap-10">
+      <h1 className="font-roboto-condensed text-6xl font-extrabold text-brand-purple-900 my-4">
+        Blog Posts
+      </h1>
+      <ul className="grid grid-cols-2 gap-8">
+        {filteredProducts.map((product) => {
+          return (
+            <li key={product.id}>
+              {product.name}
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+};
+
+export default CmsPage;
