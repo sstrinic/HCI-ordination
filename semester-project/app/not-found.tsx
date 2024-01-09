8898286f1301e5f51cb51c6f3b8888d0eafaf4c2
sstrinic <stipan.strinic.00@fesb.hr>
2024-01-09 11:13:30 +0100

    typo fixed

--- semester-project/app/not-found.tsx
+++ semester-project/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-[70vh]">
     <h1 className="text-4xl font-bold mb-4">Ooopsie doopsie &#128553;</h1>
     <h2 className="text-3xl font-bold mb-4">404 - Not Found</h2>
-    <p className="text-gray-600 mb-8">The page you're looking for does not exist.</p>
+    <p className="text-gray-600 mb-8">The page you&apos;re looking for does not exist.</p>
     <Link href="/">
       <span className="text-blue-500 hover:underline">Go back to the home page</span>
     </Link>
