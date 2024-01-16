410493b7124e0af97bdc2fdbba24ca33e7d40413
sstrinic <stipan.strinic.00@fesb.hr>
2024-01-16 23:06:47 +0100

    services style added

--- semester-project/app/services/page.tsx
+++ semester-project/app/services/page.tsx
@@ -9,29 +9,30 @@ const Services = async () => {
     <main className="max-w-[1092px] mx-auto mt-8 p-6">
       <div id='desc-top'>
             <h1 className='text-4xl font-bold mb-6 text-center'>Services</h1>
-            <h3 className='text-center'> Our mission is to provide quality service to patients both in the office and in the field. We are focused on preventive examinations as well as treatment of acute diseases and monitoring and control of chronic diseases. To achieve this, we hired the best consultants who, in cooperation with our teams, take care of your health. We have cooperation with the best laboratories in New York, and we have also organized online support and advice.</h3>
+            <h3 className='text-center max-md:text-left'> Our mission is to provide quality service to patients both in the office and in the field. We are focused on preventive examinations as well as treatment of acute diseases and monitoring and control of chronic diseases. To achieve this, we hired the best consultants who, in cooperation with our teams, take care of your health. We have cooperation with the best laboratories in New York, and we have also organized online support and advice.</h3>
           </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
-        <ul>
-      {allServices.map((service) => {
-        return (
-          <li key={service.title}>
-          <div className={styles.transformImg}>
-              <Image
-                src={service.img}
-                alt={service.title}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-md"
-              />
-              <div className={styles.serviceText}>
-                <span>{service.text}</span>
-              </div>
-          </div>
-          </li>
-        );
-      })}
-      </ul>
+      <div className='mt-4'>
+        <ul className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+          {allServices.map((service) => {
+            return (
+              <li key={service.title} className='text-center flex flex-col'>
+                <div className={styles.transformImg}>
+                    <Image
+                      src={service.img}
+                      alt={service.title}
+                      width={1000}
+                      height={1000}
+                      className="rounded-md"
+                    />
+                    <div className={styles.serviceText}>
+                      <span className='max-md:text-sm'>{service.text}</span>
+                    </div>
+                </div>
+                <p>{service.title}</p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </main>
   );
