9a6ee8b067c615ec71c92e43cf4d76849f0449b3
sstrinic <72578940+sstrinic@users.noreply.github.com>
2024-01-19 23:47:32 +0100

    Update not-found.tsx

--- semester-project/app/not-found.tsx
+++ semester-project/app/not-found.tsx
@@ -3,14 +3,16 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-[70vh]">
-      <h1 className="text-5xl font-bold mb-4">
-        Uh-oh, you&apos;ve hit a whimsical glitch &#128553;
-      </h1>
-      <h2 className="text-3xl font-bold mb-4">404 - Lost in the Digital Universe</h2>
-      <p className="text-gray-600 mb-8">Looks like the page you&apos;re seeking has gone on a cosmic adventure.</p>
-      <Link href="/">
-        <span className="text-pink-500 hover:underline">Beam me back to the home page</span>
-      </Link>
+      <div className="border border-purple-500 p-8 rounded-md text-center bg-purple-100">
+        <h1 className="text-5xl font-bold mb-4">
+          Uh-oh, you&apos;ve hit a whimsical glitch &#128553;
+        </h1>
+        <h2 className="text-3xl font-bold mb-4">404 - Lost in the Digital Universe</h2>
+        <p className="text-gray-600 mb-8">Looks like the page you&apos;re seeking has gone on a cosmic adventure.</p>
+        <Link href="/">
+          <span className="text-pink-500 hover:underline">Beam me back to the home page</span>
+        </Link>
+      </div>
     </div>
   );
 }
