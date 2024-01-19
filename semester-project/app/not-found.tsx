8124c919cd9599d576b64fdc1070553c3eff2659
sstrinic <stipan.strinic.00@fesb.hr>
2024-01-19 23:41:23 +0100

    404 page update

--- semester-project/app/not-found.tsx
+++ semester-project/app/not-found.tsx
@@ -1,13 +1,16 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-[70vh]">
-    <h1 className="text-4xl font-bold mb-4">Ooopsie doopsie &#128553;</h1>
-    <h2 className="text-3xl font-bold mb-4">404 - Not Found</h2>
-    <p className="text-gray-600 mb-8">The page you&apos;re looking for does not exist.</p>
-    <Link href="/">
-      <span className="text-blue-500 hover:underline">Go back to the home page</span>
-    </Link>
-  </div>
-)}
+      <h1 className="text-5xl font-bold mb-4">
+        Uh-oh, you've hit a whimsical glitch &#128553;
+      </h1>
+      <h2 className="text-3xl font-bold mb-4">404 - Lost in the Digital Universe</h2>
+      <p className="text-gray-600 mb-8">Looks like the page you're seeking has gone on a cosmic adventure.</p>
+      <Link href="/">
+        <span className="text-pink-500 hover:underline">Beam me back to the home page</span>
+      </Link>
+    </div>
+  );
+}
